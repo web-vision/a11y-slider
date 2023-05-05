414d18db91249edfb90649bbd9521952b22e7b29
Riad <riad@web-vision.de>
2023-05-05 12:20:50 +0200

    [TASK] Add option to show/hide title in Frontend

---
+++ Configuration/TCA/Overrides/sys_file_reference.php
@@ -0,0 +1,34 @@
+<?php
+
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
+}
+
+(static function (): void {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(
+        'sys_file_reference',
+        [
+            'tx_a11y_slider_showtitle' => [
+                'exclude' => 0,
+                'label' => 'LLL:EXT:a11y_slider/Resources/Private/Language/locallang_be.xlf:slider.images.showTitle',
+                'config' => [
+                    'type' => 'check'
+                ]
+            ],
+        ],
+    );
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+        'sys_file_reference',
+        'tx_a11y_slider_showtitle',
+        'a11y_slider',
+        'after:title'
+    );
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette(
+        'sys_file_reference',
+        'imageoverlayPalette',
+        'tx_a11y_slider_showtitle',
+        'after:title'
+    );
+})();
--- ext_tables.sql
+++ ext_tables.sql
@@ -1,3 +1,4 @@
-#
-# Add SQL definition of database tables
-#
+CREATE TABLE sys_file_reference
+(
+	tx_a11y_slider_showtitle tinyint(1) unsigned DEFAULT '0' NOT NULL,
+);
